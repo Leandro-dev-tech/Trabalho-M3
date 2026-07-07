499f5dec2014c53f5cc0f823999b526a1c331a29
Leandro de Oliveira <leandro.tsubrasa@gmail.com>
2026-07-07 19:58:23 -0300

    Create abstract MensagemDecorator class

---
+++ src/MensagemDecorator.java
@@ -0,0 +1,8 @@
+public abstract class MensagemDecorator implements Mensagem {
+
+    protected Mensagem mensagem;
+
+    public MensagemDecorator(Mensagem mensagem) {
+        this.mensagem = mensagem;
+    }
+}
